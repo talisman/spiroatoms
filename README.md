adf2d277b3f7694f1ca615f3522831c0cba012fa
Jonathan Talisman <jonathan.talisman@gmail.com>
2016-07-27 17:58:11 +0300

    Object Being clicked buildup

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # spiroatoms
 p5.js learning space
 
-- Create 'atom' symbol rotating objects , an array of objects .
+- Create 'atom' symbol rotating objects , an array of objects . - done
+- When Objects overlap - fill is off - done.
 - Change color on Object click or mouseOver .
+
 - Create Spirograph like object [circle within a circle , a point a-centric on the internal circle rotating ]
--- atomshape.js
+++ atomshape.js
@@ -7,19 +7,19 @@ function atomShape(x, y, d, h, n) {
   this.a = 0;
   this.tofill = true;
   this.col =  color(random(255),random(255),random(255),25);
-  
+
   this.rotspeed = random(0.5,3);
-  
+  this.moveSpeed = random(1.0);
+
+
   this.display = function() {
     push();
     if (this.tofill == true){
       fill(this.col);
     }
     else {
-      
       noFill();
     }
-   // fill(200,50,200,20);
     translate(this.x, this.y);
     rotate(this.a);
     for (i = 0; i < this.n; i++) {
@@ -27,14 +27,22 @@ function atomShape(x, y, d, h, n) {
       ellipse(0,0 , this.diameter, this.h)
     }
     pop();
-  this.a = this.a +this.rotspeed;
+    this.a = this.a +this.rotspeed;
   }
-  
+
   this.update = function() {
-    this.x = this.x + random(-1, 1);
-    this.y = this.y + random(-1, 1);
+    this.x = this.x + random(this.moveSpeed * -1 ,this.moveSpeed);
+    this.y = this.y + random(this.moveSpeed * -1 ,this.moveSpeed);
   }
-  
+  //
+  this.beingClicked = function(){
+    var d = dist(mouseX, mouseY, this.x, this.y);
+    if (d < this.dia) {
+     // change this to change color alpha to 1/3 of current alpha ;
+
+   }
+ }
+//
   this.intersectsWith = function(anotherAtom){
     if (dist(this.x,this.y,anotherAtom.x,anotherAtom.y) < (( this.diameter * 0.5)  +  ( anotherAtom.diameter * 0.5))){
      return true;
@@ -42,6 +50,5 @@ function atomShape(x, y, d, h, n) {
     else {
      return false;
     }
-
   }
 }
